085225c8e894ce8206ac3d3366f30f664f634cb0
juliooa <julio.olivares.a@gmail.com>
2016-03-24 09:34:47 -0300

    listado de opciones SIGA

--- app/src/main/java/cl/usm/tallerappsmoviles1/HomeActivity.java
+++ app/src/main/java/cl/usm/tallerappsmoviles1/HomeActivity.java
@@ -1,10 +1,28 @@
 package cl.usm.tallerappsmoviles1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import javax.net.ssl.CertPathTrustManagerParameters;
+
 public class HomeActivity extends AppCompatActivity {
+
+    String[] opciones = { "Horario personal",
+            "Inscripción",
+            "Certificados",
+            "Ficha personal",
+            "Resumen académico", "Encuesta docente"
+            , "Planes de Carrera","Deuda","Avance Curricular"};
+
+
+    private static final int OPCION_INSCRIPCION = 1;
+    private static final int OPCION_CERTIFICADOS = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +32,44 @@ public class HomeActivity extends AppCompatActivity {
         String nombreUsuario = getIntent().getStringExtra(MainActivity.KEY_USUARIO);
 
         TextView textViewNombreUsuario =
-                (TextView) findViewById(R.id.textViewNombreUsuario);
+                (TextView) findViewById(R.id.textViewBienvenida);
 
-        textViewNombreUsuario.setText(nombreUsuario);
+        textViewNombreUsuario.setText("Bienvenido: " + nombreUsuario);
+
+        setListViewOpciones();
+    }
+
+    private void setListViewOpciones() {
+
+        ListView listViewOpciones = (ListView) findViewById(R.id.listViewOpciones);
+
+        ArrayAdapter<String> adapterOpciones =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1,
+                        opciones);
+
+        listViewOpciones.setAdapter(adapterOpciones);
+
+        listViewOpciones.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view,
+                                    int position, long id) {
+
+                Intent intent;
+
+                switch (position){
+                    case OPCION_INSCRIPCION:
+                        intent = new Intent(HomeActivity.this,
+                                InscripcionActivity.class);
+                        startActivity(intent);
+                        break;
+                    case OPCION_CERTIFICADOS:
+                        intent = new Intent(HomeActivity.this,
+                                CertificadosActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
     }
 }
