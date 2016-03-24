1500e1ce5b9f49baa8b746abc6587a77226d1546
juliooa <julio.olivares.a@gmail.com>
2016-03-24 09:35:19 -0300

    spinner certificados

---
+++ app/src/main/java/cl/usm/tallerappsmoviles1/CertificadosActivity.java
@@ -0,0 +1,29 @@
+package cl.usm.tallerappsmoviles1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class CertificadosActivity extends AppCompatActivity {
+
+    String[] certificados = {"Alumno regular",
+            "Titulado",
+            "Egreso",
+            "Licenciado"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_certificados);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerCertificados);
+
+        ArrayAdapter<String> spinnerAdapter =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_spinner_item,certificados );
+
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setSelection(-1);
+    }
+}
