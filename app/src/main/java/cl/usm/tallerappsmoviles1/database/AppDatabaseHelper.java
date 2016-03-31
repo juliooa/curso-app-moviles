41731ff3bff677e3e6104369ed0b72f9142b5161
juliooa <julio.olivares.a@gmail.com>
2016-03-31 09:31:29 -0300

    clase base de datos SQLite

---
+++ app/src/main/java/cl/usm/tallerappsmoviles1/database/AppDatabaseHelper.java
@@ -0,0 +1,7 @@
+package cl.usm.tallerappsmoviles1.database;
+
+/**
+ * Created by JulioAndres on 3/31/16.
+ */
+public class AppDatabaseHelper {
+}
---
+++ app/src/main/java/cl/usm/tallerappsmoviles1/model/Ramo.java
@@ -0,0 +1,7 @@
+package cl.usm.tallerappsmoviles1.model;
+
+/**
+ * Created by JulioAndres on 3/31/16.
+ */
+public class Ramo {
+}
